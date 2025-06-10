eaf71c81d933dd8117296ef75dbdc8d0fe382081
Ceyda <ceydacomert1003@gmail.com>
2025-06-10 20:20:17 +0300

    Notebook App
    Not defteri uygulama projesi

---
+++ NotebookApp/NotebookApp/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace NotebookApp
+{
+    partial class NoteTaker
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.noteBox = new System.Windows.Forms.TextBox();
+            this.title = new System.Windows.Forms.Label();
+            this.note = new System.Windows.Forms.Label();
+            this.previousNoteButton = new System.Windows.Forms.Button();
+            this.newNoteButton = new System.Windows.Forms.Button();
+            this.previousNotes = new System.Windows.Forms.DataGridView();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.previousNotes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleBox
+            // 
+            this.titleBox.Location = new System.Drawing.Point(12, 42);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(340, 22);
+            this.titleBox.TabIndex = 0;
+            // 
+            // noteBox
+            // 
+            this.noteBox.Location = new System.Drawing.Point(12, 93);
+            this.noteBox.Multiline = true;
+            this.noteBox.Name = "noteBox";
+            this.noteBox.Size = new System.Drawing.Size(340, 487);
+            this.noteBox.TabIndex = 1;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("MV Boli", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(13, 13);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(64, 23);
+            this.title.TabIndex = 2;
+            this.title.Text = "Başlık";
+            // 
+            // note
+            // 
+            this.note.AutoSize = true;
+            this.note.Font = new System.Drawing.Font("MV Boli", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.note.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.note.Location = new System.Drawing.Point(13, 67);
+            this.note.Name = "note";
+            this.note.Size = new System.Drawing.Size(55, 23);
+            this.note.TabIndex = 3;
+            this.note.Text = "Not:";
+            // 
+            // previousNoteButton
+            // 
+            this.previousNoteButton.Location = new System.Drawing.Point(12, 586);
+            this.previousNoteButton.Name = "previousNoteButton";
+            this.previousNoteButton.Size = new System.Drawing.Size(120, 37);
+            this.previousNoteButton.TabIndex = 4;
+            this.previousNoteButton.Text = "Önceki Not";
+            this.previousNoteButton.UseVisualStyleBackColor = true;
+            this.previousNoteButton.Click += new System.EventHandler(this.previousNoteButton_Click);
+            // 
+            // newNoteButton
+            // 
+            this.newNoteButton.Location = new System.Drawing.Point(232, 586);
+            this.newNoteButton.Name = "newNoteButton";
+            this.newNoteButton.Size = new System.Drawing.Size(120, 37);
+            this.newNoteButton.TabIndex = 5;
+            this.newNoteButton.Text = "Yeni Not";
+            this.newNoteButton.UseVisualStyleBackColor = true;
+            this.newNoteButton.Click += new System.EventHandler(this.newNoteButton_Click);
+            // 
+            // previousNotes
+            // 
+            this.previousNotes.BackgroundColor = System.Drawing.Color.SteelBlue;
+            this.previousNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Azure;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.previousNotes.DefaultCellStyle = dataGridViewCellStyle1;
+            this.previousNotes.Location = new System.Drawing.Point(374, 12);
+            this.previousNotes.Name = "previousNotes";
+            this.previousNotes.RowHeadersWidth = 51;
+            this.previousNotes.RowTemplate.Height = 24;
+            this.previousNotes.Size = new System.Drawing.Size(339, 438);
+            this.previousNotes.TabIndex = 6;
+            this.previousNotes.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.previousNotes_CellDoubleClick);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(374, 456);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(194, 37);
+            this.saveButton.TabIndex = 7;
+            this.saveButton.Text = "Kaydet";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(374, 499);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(194, 37);
+            this.deleteButton.TabIndex = 8;
+            this.deleteButton.Text = "Sil";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(374, 542);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(194, 37);
+            this.loadButton.TabIndex = 9;
+            this.loadButton.Text = "Aç";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // NoteTaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(725, 635);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.previousNotes);
+            this.Controls.Add(this.newNoteButton);
+            this.Controls.Add(this.previousNoteButton);
+            this.Controls.Add(this.note);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.noteBox);
+            this.Controls.Add(this.titleBox);
+            this.Name = "NoteTaker";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.NoteTaker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.previousNotes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.TextBox noteBox;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label note;
+        private System.Windows.Forms.Button previousNoteButton;
+        private System.Windows.Forms.Button newNoteButton;
+        private System.Windows.Forms.DataGridView previousNotes;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button loadButton;
+    }
+}
+
---
+++ NotebookApp/NotebookApp/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotebookApp
+{
+    public partial class NoteTaker : Form
+    {
+        DataTable notes = new DataTable();
+        bool editing = false;
+
+        public NoteTaker()
+        {
+            InitializeComponent();
+        }
+
+        private void NoteTaker_Load(object sender, EventArgs e)
+        {
+            notes.Columns.Add("Title");
+            notes.Columns.Add("Note");
+
+            previousNotes.DataSource = notes;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+            if (previousNotes.CurrentCell != null &&
+        previousNotes.CurrentCell.RowIndex >= 0 &&
+        notes.Rows.Count > previousNotes.CurrentCell.RowIndex)
+            {
+                int rowIndex = previousNotes.CurrentCell.RowIndex;
+                titleBox.Text = notes.Rows[rowIndex].ItemArray[0].ToString();
+                noteBox.Text = notes.Rows[rowIndex].ItemArray[1].ToString(); // not içeriği olduğunu varsayarsak
+                editing = true;
+            }
+            else
+            {
+                MessageBox.Show("Yüklenecek bir not seçilmedi veya not listesi boş.",
+                                "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (previousNotes.CurrentCell != null && previousNotes.Rows.Count > 0)
+                {
+                    int rowIndex = previousNotes.CurrentCell.RowIndex;
+
+                    if (rowIndex >= 0 && rowIndex < notes.Rows.Count)
+                    {
+                        notes.Rows[rowIndex].Delete();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Silinecek geçerli bir not seçili değil.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Silinecek not bulunamadı.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir hata oluştu: " + ex.Message);
+            }
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            if (previousNotes.CurrentCell != null &&
+        notes.Rows.Count > previousNotes.CurrentCell.RowIndex &&
+        previousNotes.CurrentCell.RowIndex >= 0)
+            {
+                titleBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+                noteBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[1].ToString(); // Sanırım burası 1 olmalı, başlık ile not farklı olabilir
+                editing = true;
+            }
+            else
+            {
+                MessageBox.Show("Yüklenebilecek not bulunamadı.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void newNoteButton_Click(object sender, EventArgs e)
+        {
+            titleBox.Text = "";
+            noteBox.Text = "";
+            editing = false;
+        }
+
+        private void previousNoteButton_Click(object sender, EventArgs e)
+        {
+            // DataTable'da en az 2 kayıt olup olmadığını kontrol etsin
+            if (notes.Rows.Count < 2 || previousNotes.CurrentCell == null)
+            {
+                MessageBox.Show("Önceki not yok veya seçim yapılmamış.");
+                return;
+            }
+
+            int currentIndex = previousNotes.CurrentCell.RowIndex;
+            int previousIndex = currentIndex - 1;
+
+            // previousIndex geçerli bir satır mı?
+            if (previousIndex >= 0)
+            {
+                titleBox.Text = notes.Rows[previousIndex]["Title"].ToString();
+                noteBox.Text = notes.Rows[previousIndex]["Note"].ToString();
+                editing = true;
+
+                previousNotes.CurrentCell = previousNotes.Rows[previousIndex].Cells[0];
+            }
+            else
+            {
+                MessageBox.Show("Daha önceki bir not bulunamadı.");
+            }
+        }
+
+        private void previousNotes_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && notes.Rows.Count > e.RowIndex)
+            {
+                titleBox.Text = notes.Rows[e.RowIndex].ItemArray[0].ToString();
+                noteBox.Text = notes.Rows[e.RowIndex].ItemArray[1].ToString(); // Not kısmı olduğunu varsayarsak 1. sütun
+                editing = true;
+            }
+            else
+            {
+                MessageBox.Show("Geçerli bir not seçilmedi.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
